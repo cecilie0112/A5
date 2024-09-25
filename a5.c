8e3866d28b7ea588a2dd2c1e25cd3d7119995f2f
Cecilie Zhang <zhan4650@eceprog3.ecn.purdue.edu>
2024-09-25 00:16:26 -0400

    tried to filter away points that were larger than the radius + the center

--- a5.c
+++ a5.c
@@ -5,7 +5,7 @@
 #define COL 2
 #define ROW 100
 
-int readFile(char * pointFile, int(**)[COL], int, int *);
+int readFile(char * pointFile, int(**)[COL]);
 int calculateCirc(int, int, int, int, int);
 
 int main(int argc, char * * argv)
@@ -20,32 +20,21 @@ int main(int argc, char * * argv)
     // int inputFlag; 
     int (*pointArray)[COL];
     pointArray = malloc(sizeof(*pointArray) * ROW);
+    int rowIndx = readFile(argv[1], &pointArray);
 
     int inputFlag, xCent, yCent, radius;
     int col = 0;
     int count;
-    int flag = 0;
     do{
       count = 0;
       inputFlag = scanf("%d %d %d", &xCent, &yCent, &radius);
-      int rowIndx = readFile(argv[1], &pointArray, radius, &flag);
-      if (!flag) {
-        printf("No points in the given circle!\n");
-        free(pointArray);
-        return EXIT_FAILURE;
-      }
-
-    //   for (int i = 0; i < 8; i++) {
-    //     for (int j = 0; j < 2; j++) {
-    //         printf("%d ", pointArray[i][j]);
-    //     }
-    //     printf("\n");
-    //   }
       
       int radiusSquared = radius * radius;
       for (int row = 0; row < rowIndx; row++) {
-        if (calculateCirc(pointArray[row][col], pointArray[row][col + 1], xCent, yCent, radiusSquared)) {
-          count++;
+        if ((abs(pointArray[row][col]) <= (radius + xCent)) && abs(pointArray[row][col + 1]) <= (radius + yCent)){
+          if (calculateCirc(pointArray[row][col], pointArray[row][col + 1], xCent, yCent, radiusSquared)) {
+            count++;
+          }
         }
       }
       
@@ -62,7 +51,7 @@ int main(int argc, char * * argv)
 } 
 
 
-int readFile(char * pointFile, int(**pointArray)[COL], int radius, int * flag) {
+int readFile(char * pointFile, int(**pointArray)[COL]) {
   FILE * fptr = fopen(pointFile, "r");
   if (fptr == NULL) {
     printf("Unable to open file");
@@ -83,13 +72,9 @@ int readFile(char * pointFile, int(**pointArray)[COL], int radius, int * flag) {
           currentSize = currentIndx * 2;
         }
     }
-
-    if (abs(xPoint) <= radius && abs(yPoint) <= radius){
-        *flag = 1;
-        (*pointArray)[currentIndx][0] = xPoint;
-        (*pointArray)[currentIndx][1] = yPoint;
-        ++currentIndx;
-    }
+    (*pointArray)[currentIndx][0] = xPoint;
+    (*pointArray)[currentIndx][1] = yPoint;
+    ++currentIndx;
   }
 
   fclose(fptr);
