793f1c8d70ca0b73a0571df89cbe6236f95098f0
Cecilie Zhang <zhan4650@eceprog2.ecn.purdue.edu>
2024-09-26 01:38:54 -0400

    The version that passed test cases with memory leaks

--- a5.c
+++ a5.c
@@ -6,26 +6,18 @@ typedef struct tnode
   int value[2];
   struct tnode * left;
   struct tnode * right;
-  int height; //Keep track of the height of the tree to make it balanced
 } TreeNode;
 
 typedef struct trnode
 {
   TreeNode * root;
 } Tree;
-//For this program, I'm creating an AVL tree instead of a regular binary tree to keep it balanced and reduce the time complexity to O(n)
 
 Tree * buildTree(int, int, Tree *);
 TreeNode * builderHelp (int, int, TreeNode *);
 TreeNode * newNode(int, int);
 void delTree(TreeNode *);
 void findCount(TreeNode *, int, int, int, int *, int, int);
-
-int findMax(int, int);
-int getNodeHeight(TreeNode *);
-int findBalanceFactor(TreeNode *);
-TreeNode * leftRotation (TreeNode *);
-TreeNode * rightRotation(TreeNode *);
 
 int main(int argc, char * * argv)
     //argv[0]: ./a5
@@ -75,11 +67,6 @@ int main(int argc, char * * argv)
     fclose(fptr);
     return EXIT_SUCCESS;
 } 
-
-int findMax(int x, int y) {
-    if (x > y) {return x;}
-    else {return y;}
-}
 
 void delTree(TreeNode * node) {
     if (node == NULL) {
@@ -132,76 +119,12 @@ TreeNode * builderHelp (int xPoint, int yPoint, TreeNode * node) {
     else if (node -> value[0] >= xPoint) {
         node -> left = builderHelp(xPoint, yPoint, node -> left);
     }
+
     else if (node -> value[0] < xPoint) {
-        node -> right = builderHelp(xPoint, yPoint, node -> right);
-    }
-
-    //Finding the max height of the left and right sub tree. Adding 1 to account for the root. 
-    node -> height = 1 + findMax(getNodeHeight(node -> left), getNodeHeight(node -> right)); 
-    int balanceFactor = findBalanceFactor(node); //Finding the balance factor
-
-    //Left rotation 
-    if ((balanceFactor <= -2) && (xPoint >= node -> right -> value[0])) {
-        return (leftRotation(node));
-    }
-    //Right rotation
-    if ((balanceFactor >= 2) && (xPoint <= node -> left -> value[0])) {
-        return (rightRotation(node));
-    }
-    //First left rotation --> then right rotation
-    if ((balanceFactor >= 2) && (xPoint >= node -> left -> value[0])) {
-        node -> left = leftRotation(node -> left);
-        return (rightRotation(node));
-    }
-    //First right rotation --> then left rotation
-    if ((balanceFactor <= -2) && (xPoint <= node -> right -> value[0])) {
-        node -> right = rightRotation(node -> right);
-        return (leftRotation(node));
+        node -> right = builderHelp(xPoint, yPoint, node -> right);;
     }
 
     return (node);
-}
-
-TreeNode * leftRotation (TreeNode * node) {
-    TreeNode * newRoot = node -> right;
-    TreeNode * tempLeft = newRoot -> left;
-
-    newRoot -> left = node;
-    node -> right = tempLeft;
-
-    node -> height = findMax(getNodeHeight(node -> left), getNodeHeight(node -> right)) + 1;
-    newRoot -> height = findMax(getNodeHeight(newRoot -> left), getNodeHeight(newRoot -> right)) + 1; 
-
-    return newRoot;
-}
-
-TreeNode * rightRotation(TreeNode * node) {
-    TreeNode * newRoot = node -> left;
-    TreeNode * tempRight = newRoot -> right;
-
-    newRoot -> right = node;
-    node -> left = tempRight;
-
-    node -> height = findMax(getNodeHeight(node -> left), getNodeHeight(node -> right)) + 1;
-    newRoot -> height = findMax(getNodeHeight(newRoot -> left), getNodeHeight(newRoot -> right)) + 1; 
-
-    return newRoot;
-}
-
-int findBalanceFactor(TreeNode * node) {
-    if (node == NULL) {
-        return 0;
-    }
-    else {
-        return (getNodeHeight(node -> left) - getNodeHeight(node -> right));
-    }
-}
-
-int getNodeHeight(TreeNode * node) {
-    if (node == NULL) {
-        return 0;
-    }
-    return (node -> height);
 }
 
 TreeNode * newNode(int xPoint, int yPoint) {
@@ -212,6 +135,5 @@ TreeNode * newNode(int xPoint, int yPoint) {
 
     node -> left = NULL;
     node -> right = NULL;
-    node -> height = 1; //Initializing the height of the node to be 1 for the calculation of the balacing factor later
     return node;
 }
